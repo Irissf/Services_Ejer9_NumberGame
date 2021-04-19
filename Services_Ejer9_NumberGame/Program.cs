408f65e4f61d585782952f63fe5805fa6c851726
Iris SF <iris.kosy@gmail.com>
2021-04-19 12:35:22 +0200

    Agregar archivos de proyecto.

---
+++ Services_Ejer9_NumberGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services_Ejer9_NumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any,31416);
+            Socket socketServe = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socketServe.Bind(ipEndPoint);
+                socketServe.Listen(3);
+                
+                //Cuando aceptemos a un usuario lo metemos en un hilo
+            }
+            catch (SocketException e) when (e.ErrorCode == (int)SocketError.AddressAlreadyInUse)
+            {
+                Console.WriteLine("Port already in use");
+            }
+        }
+    }
+}
