d757a5588400a3137b5d1dd3cc75e020f257ba61
Iris SF <iris.kosy@gmail.com>
2021-04-22 10:45:44 +0200

    TT.TT

---
+++ Services_Ejer9_NumberGame/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services_Ejer9_NumberGame
+{
+    class Player
+    {
+        public int Number { set; get; }
+        public Socket SocketPlayer { set; get; }
+       
+        //public string Name { set; get; }
+
+        public Player(int number, Socket socketPlayer)
+        {
+            this.Number = number;
+            this.SocketPlayer = socketPlayer;
+        }
+    }
+}
--- Services_Ejer9_NumberGame/Program.cs
+++ Services_Ejer9_NumberGame/Program.cs
@@ -1,31 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+/*Juego número más alto en red. Al servidor se conectan al menos dos clientes (puede haber más) y les da un 
+número aleatorio entre 1 y 20. Cuando todos los clientes estén conectados (Hazlo por tiempo. Que los 
+clientes vean el tiempo que queda para empezar) el servidor comprueba quién es el ganador e informa de 
+ello a los clientes. Debe indicar a los que pierde el número ganador además del número que sacaron.*/
 
 namespace Services_Ejer9_NumberGame
 {
     class Program
     {
+
+        static List<Player> playerList = new List<Player>();
+        static Random randomNumber = new Random();
+        static bool gameStart = false;
+        static int timeToStartGame = 10;//descendemos a 0
+        static object key = new object();
         static void Main(string[] args)
         {
-            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any,31416);
+
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 31416);
             Socket socketServe = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
             {
                 socketServe.Bind(ipEndPoint);
                 socketServe.Listen(3);
-                
+
                 //Cuando aceptemos a un usuario lo metemos en un hilo
+                while (!gameStart)
+                {
+                    Socket socketClient = socketServe.Accept();
+
+                    //creamos un hilo para el cliente conectado
+                    Thread thread = new Thread(Game);
+                    thread.Start(socketClient);
+                    
+                }
             }
             catch (SocketException e) when (e.ErrorCode == (int)SocketError.AddressAlreadyInUse)
             {
                 Console.WriteLine("Port already in use");
             }
+
+        }
+
+        static void Game(object socket)
+        {
+            Socket socketPlayer = (Socket)socket;
+            playerList.Add(new Player(randomNumber.Next(1, 21), socketPlayer));
+
+            using (NetworkStream ns = new NetworkStream(socketPlayer))
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                lock (key)
+                {
+                    //El elemento común es la consola por la que saldrá los mensajes
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("num usuarios:" + playerList.Count);
+                }
+                
+                if (playerList.Count >= 2)
+                {
+                    //si ya hay dos jugadores lanzamos la cuenta atrás y el timer al llegar a 0 cambia gamestart a true para que no entren más jugadores
+                    Thread threadTimer = new Thread(timerGame);
+                    threadTimer.Start();
+
+                }
+            }
+        }
+
+        static void timerGame()
+        {
+            
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                
+                Console.WriteLine("Entro aqui");
+                using (NetworkStream ns = new NetworkStream(playerList[i].SocketPlayer))
+                using (StreamReader sr = new StreamReader(ns))
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    while (timeToStartGame > 0)
+                    {
+                        Thread.Sleep(1000);
+                        lock (key)
+                        {
+                            //variable común para más de un método
+                            timeToStartGame--;
+
+                            sw.WriteLine("Time to start: {0}", timeToStartGame);
+                        }
+                    }
+                   
+                }
+            }
+
         }
     }
 }
